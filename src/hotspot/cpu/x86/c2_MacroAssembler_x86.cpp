openjdk/jdk

8316179: Use consistent naming for lightweight locking in MacroAssembler
Reviewed-by: rkennke, coleenp, dholmes

src/hotspot/cpu/x86/c2_MacroAssembler_x86.cpp
@@ -622,7 +622,7 @@ void C2_MacroAssembler::fast_lock(Register objReg, Register boxReg, Register tmp
     movptr(Address(boxReg, 0), tmpReg);
   } else {
     assert(LockingMode == LM_LIGHTWEIGHT, "");
-    fast_lock_impl(objReg, tmpReg, thread, scrReg, NO_COUNT);
+    lightweight_lock(objReg, tmpReg, thread, scrReg, NO_COUNT);
     jmp(COUNT);
   }
   jmp(DONE_LABEL);
@@ -926,7 +926,7 @@ void C2_MacroAssembler::fast_unlock(Register objReg, Register boxReg, Register t
     bind  (Stacked);
     if (LockingMode == LM_LIGHTWEIGHT) {
       mov(boxReg, tmpReg);
-      fast_unlock_impl(objReg, boxReg, tmpReg, NO_COUNT);
+      lightweight_unlock(objReg, boxReg, tmpReg, NO_COUNT);
       jmp(COUNT);
     } else if (LockingMode == LM_LEGACY) {
       movptr(tmpReg, Address (boxReg, 0));      // re-fetch

rkennke/lilliput-jdk17u

src/hotspot/cpu/x86/c2_MacroAssembler_x86.cpp
@@ -545,7 +545,7 @@ void C2_MacroAssembler::fast_lock(Register objReg, Register boxReg, Register tmp
     }
   } else {
     assert(LockingMode == LM_LIGHTWEIGHT, "");
-    fast_lock_impl(objReg, tmpReg, thread, scrReg, DONE_LABEL);
+    lightweight_lock(objReg, tmpReg, thread, scrReg, DONE_LABEL);
     xorl(tmpReg, tmpReg); // Set ZF=1 to indicate success
   }
   jmp(DONE_LABEL);
@@ -859,7 +859,7 @@ void C2_MacroAssembler::fast_unlock(Register objReg, Register boxReg, Register t
     bind  (Stacked);
     if (LockingMode == LM_LIGHTWEIGHT) {
       mov(boxReg, tmpReg);
-      fast_unlock_impl(objReg, boxReg, tmpReg, DONE_LABEL);
+      lightweight_unlock(objReg, boxReg, tmpReg, DONE_LABEL);
       xorl(tmpReg, tmpReg);
     } else if (LockingMode == LM_LEGACY) {
       movptr(tmpReg, Address (boxReg, 0));      // re-fetch